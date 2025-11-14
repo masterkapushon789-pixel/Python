5c088e00c81fe697c0210bb1d318555e9c0ca85c
fucking liar <liar.pimply@gmail.com>
2025-11-14 20:16:10 +0300

    Обновлен статус задания 14 темы 5. Задание решено Неверно

---
+++ ЕГЭ/Тема 5/Задания/-Задание 14.py
@@ -0,0 +1,21 @@
+# Решение
+a = []
+for n in range(1, 50):
+    n2 = bin(n)[2:]
+    n2 += bin(n % 4)[2:]
+    r = int(n2, 2)
+    a.append(r)
+print(len(a))
+
+
+
+
+
+
+answer = 999
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(5, 14, answer, '1f0e3dad99908345f7439f8ffabdffc4'))
