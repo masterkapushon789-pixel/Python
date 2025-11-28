f602c446d65d800dbe76e64e204752b2d45a9764
fucking liar <liar.pimply@gmail.com>
2025-11-28 22:27:10 +0300

    Обновлен статус задания 52 темы 5. Задание решено Неверно

--- ЕГЭ/Тема 5/Задания/-Задание 52.py
+++ ЕГЭ/Тема 5/Задания/-Задание 52.py
@@ -9,17 +9,10 @@ for n in range(2, 100):
     for i in range(0, len(n2), 2):
         if n2[i] == '0':
             b += 1
-    r = abs(a - b)
+    r = abs(b - a)
     if r == 5:
         print(n)
         break
-
-
-
-
-
-
-
 
 answer = 15
 
