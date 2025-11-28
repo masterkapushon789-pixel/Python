75e2518a7549920949ca097c64a09a92a7e9b486
fucking liar <liar.pimply@gmail.com>
2025-11-28 22:25:57 +0300

    Обновлен статус задания 52 темы 5. Задание решено Неверно

---
+++ ЕГЭ/Тема 5/Задания/-Задание 52.py
@@ -0,0 +1,30 @@
+# Решение
+a = 0
+b = 0
+for n in range(2, 100):
+    n2 = bin(n)[2:]
+    for i in range(1, len(n2), 2):
+        if n2[i] == '1':
+            a += 1
+    for i in range(0, len(n2), 2):
+        if n2[i] == '0':
+            b += 1
+    r = abs(a - b)
+    if r == 5:
+        print(n)
+        break
+
+
+
+
+
+
+
+
+answer = 15
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(5, 52, answer, 'ce5140df15d046a66883807d18d0264b'))
