de25260f7e53bbaa4be946efa697c42faeaf27b4
Олег <liar.pimply@gmail.com>
2026-01-09 22:50:49 +0300

    Обновлен статус задания 16 темы 1. Задание решено Неверно

---
+++ ЕГЭ/Тема 1/Задания/-Задание 16.py
@@ -0,0 +1,25 @@
+# Решение
+from itertools import permutations
+
+table =     '15 18 23 25 32 35 36 45 47 51 52 53 54 56 57 58 63 65 74 75 78 81 85 87'
+artemgraf = 'ab ac ad ae af ag ah bc ba cd cb ca dc da ef ea fe fg fa gf gh ga hg ha'
+
+for p in permutations('abcdefgh'):
+    newartemgraf = table
+    for i in range(1, 9):
+        newartemgraf = newartemgraf.replace(str(i), p[i - 1])
+    if set(newartemgraf.split()) == set(artemgraf.split()):
+        print(p)
+
+
+
+
+
+
+answer = 51
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(1, 16, answer, '642e92efb79421734881b53e1e1b18b6'))
