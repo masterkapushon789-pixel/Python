8b91ee4277b14f4b4294c1c942e67b237f57d006
Олег <liar.pimply@gmail.com>
2026-01-10 00:39:55 +0300

    Обновлен статус задания 7 темы 7. Задание решено Верно

---
+++ ЕГЭ/Тема 7/Задания/+Задание 7.py
@@ -0,0 +1,15 @@
+# Решение
+import math
+n = 192 * 960
+v = 90 * 2 ** 13 / 0.65
+i = v / n
+i = math.floor(i)
+
+
+answer = 2 ** i
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(7, 7, answer, 'ea5d2f1c4608232e07d3aa3d998e5135'))
