4f94bb34aa99b10f053545426dd0ca95a6e05e78
Олег <liar.pimply@gmail.com>
2026-01-10 14:25:33 +0300

    Обновлен статус задания 16 темы 1. Задание решено Верно

--- ЕГЭ/Тема 1/Задания/+Задание 16.py
+++ ЕГЭ/Тема 1/Задания/+Задание 16.py
@@ -3,7 +3,7 @@ from itertools import permutations
 
 table =     '15 18 23 25 32 35 36 45 47 51 52 53 54 56 57 58 63 65 74 75 78 81 85 87'
 artemgraf = 'ab ac ad ae af ag ah bc ba cd cb ca dc da ef ea fe fg fa gf gh ga hg ha'
-
+print('  1    2    3    4    5    6    7    8')
 for p in permutations('abcdefgh'):
     newartemgraf = table
     for i in range(1, 9):
