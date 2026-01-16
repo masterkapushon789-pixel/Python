e321a03652ff97257ffb50c6650449ce3295572d
Олег <liar.pimply@gmail.com>
2026-01-16 23:55:21 +0300

    Обновлен статус задания № 24 Тема: 2 > Неверно

--- ЕГЭ/Тема 2/Задания/-Задание 24.py
+++ ЕГЭ/Тема 2/Задания/-Задание 24.py
@@ -3,7 +3,7 @@ from itertools import product
 
 print('w x y z')
 for w, x, y, z in product([0, 1], repeat=4):
-    if (x == (y <= z)) and (y == (not(z <= w))):
+    if (x == (y <= z)) and (y == (not (z <= w))):
         print(w, x, y, z)
 
 
